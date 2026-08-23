d1149d79f30ce07f9bb6418af63ea4d127dfc8a2
purplehat93 <69501431+purplehat93@users.noreply.github.com>
2026-08-23 20:20:53 +0300

    fix: make type-aware linting compatible with tests

--- eslint.config.js
+++ eslint.config.js
@@ -12,8 +12,7 @@ export default tseslint.config(
       },
     },
     rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/no-floating-promises': 'error'
+      '@typescript-eslint/no-floating-promises': 'error',
     },
   },
   {
